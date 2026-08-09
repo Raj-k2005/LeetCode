ac17f150fb6a84ec45cfce8b4c011060067fbd64
Raj-k2005 <basukalakoti@gmail.com>
2026-08-09 22:47:19 +0530

    Add solution for Palindrome Number

---
+++ solutions/easy/Palindrome Number.java
@@ -0,0 +1,19 @@
+// Title: Palindrome Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/palindrome-number/
+
+public static boolean isPalindrome(int num) {
+    int original = num;
+    int reversed = 0;
+
+    while (num > 0) {
+        int digit = num % 10;
+        reversed = reversed * 10 + digit;
+        num /= 10;
+    }
+
+    return original == reversed;
+}
+
+}
