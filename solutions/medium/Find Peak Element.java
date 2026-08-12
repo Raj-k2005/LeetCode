c2be4b01443c7062bfe352de94ff80f12514ebc6
Raj-k2005 <basukalakoti@gmail.com>
2026-08-12 22:48:26 +0530

    Add solution for Find Peak Element

---
+++ solutions/medium/Find Peak Element.java
@@ -0,0 +1,22 @@
+// Title: Find Peak Element
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-peak-element/
+
+
+        while(start < end){
+            int mid=start+(end-start)/2;
+        }
+            if(arr[mid] > arr[mid+1]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+    }
+        return start;
+}
+        int end=arr.length-1;
+        int start=0;
+    public int findPeakElement(int[] arr) {
+class Solution {
