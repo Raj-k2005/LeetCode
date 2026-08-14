b246aeb329354dc240cf2c919578b43ca6c50191
Raj-k2005 <basukalakoti@gmail.com>
2026-08-14 20:25:04 +0530

    Add solution for Guess Number Higher or Lower

---
+++ solutions/easy/Guess Number Higher or Lower.java
@@ -0,0 +1,23 @@
+// Title: Guess Number Higher or Lower
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/guess-number-higher-or-lower/
+
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int guessres=guess(mid);
+
+        int right=n;
+        int left=1;
+//Time complexity:O(logn)
+//Space complexity:O(1)
+    public int guessNumber(int n) {
+public class Solution extends GuessGame {
+
+ */
+ * int guess(int num);
+            if(guessres==0){
+                return mid;
+            }
+            else if(guessres==-1){
