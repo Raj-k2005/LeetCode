679b8cb7fe3af72acd833930ea041acb74d08456
Raj-k2005 <basukalakoti@gmail.com>
2026-08-14 22:46:04 +0530

    Add solution for First Bad Version

---
+++ solutions/easy/First Bad Version.java
@@ -0,0 +1,22 @@
+// Title: First Bad Version
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/first-bad-version/
+
+                right=mid-1;
+            }
+            boolean res=isBadVersion(mid);
+
+            if(res){
+                fdb=mid;
+        int left=1;
+        int right=n;
+        int fdb=-1;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+      boolean isBadVersion(int version); */
+
+/* The isBadVersion API is defined in the parent class VersionControl.
