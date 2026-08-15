b5c1f74d960a8343288cfa4e171986876265fae1
Raj-k2005 <basukalakoti@gmail.com>
2026-08-15 19:32:26 +0530

    Add solution for Find Minimum in Rotated Sorted Array

--- solutions/medium/Find Minimum in Rotated Sorted Array.java
+++ solutions/medium/Find Minimum in Rotated Sorted Array.java
@@ -3,8 +3,13 @@
             // Language: Java
             // Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
+class Solution {
+    public int findMin(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+
+        while(left<right){
             int mid=left+(right-left)/2;
-        }
 
             if(nums[mid]>nums[right]){
                 left=mid+1;
@@ -12,6 +17,5 @@
             else{
                 right=mid;
             }
+        }
         return nums[left];
-    }
-}
