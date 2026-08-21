699e933c37277ff21a07fb793295f979b129eb84
Raj-k2005 <basukalakoti@gmail.com>
2026-08-21 19:44:26 +0530

    Add solution for Majority Element

---
+++ solutions/easy/Majority Element.java
@@ -0,0 +1,22 @@
+// Title: Majority Element
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/majority-element/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cand=0;
+        int count=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                cand=nums[i];
+            }
+            if(cand==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return cand;
