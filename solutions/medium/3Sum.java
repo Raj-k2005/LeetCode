65a33a58a778c735a6356c4bc9ca22c71d751675
Raj-k2005 <basukalakoti@gmail.com>
2026-08-27 20:40:02 +0530

    Add solution for 3Sum

---
+++ solutions/medium/3Sum.java
@@ -0,0 +1,22 @@
+// Title: 3Sum
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/3sum/
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list=new ArrayList<>();
+        for(int i=0;i<nums.length-2;i++)
+        {
+                if(i>0 && nums[i]==nums[i-1])
+                {
+                    continue;
+                }
+               int j=i+1;
+               int k=nums.length-1;
+               while(j<k)
+               {
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum<0)
+                {
