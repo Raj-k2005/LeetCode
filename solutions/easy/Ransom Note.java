be8248683ff23cb0117aa447731b773f1e52389e
Raj-k2005 <basukalakoti@gmail.com>
2026-08-29 20:37:02 +0530

    Add solution for Ransom Note

---
+++ solutions/easy/Ransom Note.java
@@ -0,0 +1,21 @@
+// Title: Ransom Note
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String 
+    magazine) {
+        int[] count=new int[26];
+        for(char c:magazine.toCharArray()){
+            count[c-'a']++;
+        }
+        for(char c:ransomNote.toCharArray()){
+            if(count[c-'a']==0){
+                return false;
+            }
+            count[c-'a']--;
+        }
+        return true;
+    }
+}
