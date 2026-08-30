37004c2b841d2fd8ef2929c5e5eaa6e7a764c3e7
basu_kalakoti <basukalakoti@gmail.com>
2026-08-30 19:47:39 +0530

    Add solution for Isomorphic Strings

---
+++ solutions/easy/Isomorphic Strings.java
@@ -0,0 +1,21 @@
+// Title: Isomorphic Strings
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/isomorphic-strings/
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> hm=new 
+        HashMap<Character,Character>();
+        for(int i=0;i<s.length();i++){
+            if(hm.containsKey(s.charAt(i))){
+                if(!hm.get(s.charAt(i)).equals(t.
+                charAt(i))){
+                               return false;   
+                }
+            }
+            else{
+                if(hm.containsValue(t.charAt(i))){
+                    return false;
+                }
+                hm.put(s.charAt(i),t.charAt(i));
