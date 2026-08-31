451b4bebed61217e9eab6dabd7083b856a612f1e
basu_kalakoti <basukalakoti@gmail.com>
2026-08-31 20:58:54 +0530

    Add solution for Group Anagrams

--- solutions/medium/Group Anagrams.java
+++ solutions/medium/Group Anagrams.java
@@ -3,15 +3,19 @@
             // Language: Java
             // Link: https://leetcode.com/problems/group-anagrams/
 
+class Solution {
+    public List<List<String>> groupAnagrams(String
+    [] strs) {
+        Map<String, List<String>> map = new 
+        HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
             String key = new String(chars);
 
             if (!map.containsKey(key)) {
                 map.put(key, new ArrayList<>());
             }
 
-            map.get(key).add(str);
-        }
-
-        return new ArrayList<>(map.values());
-    }
-}
