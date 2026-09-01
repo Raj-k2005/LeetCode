062ac3ce8a2c9b5b718ae9f09edcf3491e44f6d8
basu_kalakoti <basukalakoti@gmail.com>
2026-09-01 20:40:31 +0530

    Add solution for 3Sum Closest

--- solutions/medium/3Sum Closest.java
+++ solutions/medium/3Sum Closest.java
@@ -5,17 +5,17 @@
 
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        //sort the array 
         Arrays.sort(nums);
         int resum=nums[0]+nums[1]+nums[2];
         int minDiff=Integer.MAX_VALUE;
 
+// Iterate the loop till the length
         for(int i=0;i<nums.length;i++){
             int left=i+1;
             int right=nums.length-1;
 
+//check for the target elements both the side
             while(left<right){
+                //add the elements of i then left and right
                 int sum=nums[i]+nums[left]+nums[right];
-
-                if(sum==target){
-                    return target;
-                }
