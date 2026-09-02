b952bbcac9ef04b4f0bc1516213fb77ef5600ade
basu_kalakoti <basukalakoti@gmail.com>
2026-09-02 20:42:32 +0530

    Add solution for 4Sum

--- solutions/medium/4Sum.java
+++ solutions/medium/4Sum.java
@@ -4,18 +4,17 @@
             // Link: https://leetcode.com/problems/4sum/
 
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public List<List<Integer>> fourSum(int[] nums, int 
+    target) {
         List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
         int n=nums.length;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                long target2=(long)target-(long)nums[i]-(long)nums[j];
+                long target2=(long)target-(long)nums[i]-
+                (long)nums[j];
                 int left=j+1;
                 int right=n-1;
                 while(left<right){
                     int s=nums[left]+nums[right];
                     if(s<target2){
-                        left++;
-                    }
-                    else if(s>target2){
