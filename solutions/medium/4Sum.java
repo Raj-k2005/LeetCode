4492931f7b7e623157c5d555aae4721b25ab82c6
basu_kalakoti <basukalakoti@gmail.com>
2026-09-02 21:06:20 +0530

    Add solution for 4Sum

--- solutions/medium/4Sum.java
+++ solutions/medium/4Sum.java
@@ -4,6 +4,8 @@
             // Link: https://leetcode.com/problems/4sum/
 
 class Solution {
+    //sort the array first and take a new list to store the 
+    elements
     public List<List<Integer>> fourSum(int[] nums, int 
     target) {
         List<List<Integer>> ans=new ArrayList<>();
@@ -11,10 +13,8 @@ class Solution {
         int n=nums.length;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
+                //cheeck for the target element 
                 long target2=(long)target-(long)nums[i]-
                 (long)nums[j];
                 int left=j+1;
                 int right=n-1;
-                while(left<right){
-                    int s=nums[left]+nums[right];
-                    if(s<target2){
