d0ae458a13e1b255d97f660c020a8d8a542fe4e9
basu_kalakoti <basukalakoti@gmail.com>
2026-09-03 21:38:31 +0530

    Add solution for Longest Repeating Character Replacement

---
+++ solutions/medium/Longest Repeating Character Replacement.java
@@ -0,0 +1,21 @@
+// Title: Longest Repeating Character Replacement
+            // Difficulty: Medium
+            // Language: Java
+            // Link: https://leetcode.com/problems/longest-repeating-character-replacement/
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq=new int[26];
+        int left=0;
+        int maxFreq=0;
+        int maxWindow=0;
+
+        for(int right=0;right<s.length();right++){
+            freq[s.charAt(right)-'A']++;
+
+            maxFreq=Math.max(maxFreq, freq[s.charAt(right)-'A']);
+
+            int windowLength=right-left+1;
+
+            if(windowLength-maxFreq>k){
+                freq[s.charAt(left)-'A']--;
