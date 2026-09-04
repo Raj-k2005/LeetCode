e51a28c22ccbd2f76e68f95ad1030ef0b4ec67e2
basu_kalakoti <basukalakoti@gmail.com>
2026-09-04 23:02:05 +0530

    Add solution for Permutation in String

--- solutions/medium/Permutation in String.java
+++ solutions/medium/Permutation in String.java
@@ -9,13 +9,13 @@ class Solution {
         int m=s2.length();
         if(m<n) return false;
         int map1[]= new int[26];
+        int map2[]= new int[26];
 
         for(int i=0;i<s1.length();i++){
             map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++;
 
         }
-        for(int i=0;i<=m-n;i++){
-           int map2[]= new int[26];
-           for(int j=0;j<n;j++){
-            map2[s2.charAt(i+j)-'a']++;
+        if(isMatched(map1, map2)){
+            return true;
            }
