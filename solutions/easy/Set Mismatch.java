58affedcf6b80dc47b60c57eed037955bd9b527d
basu_kalakoti <basukalakoti@gmail.com>
2026-09-12 20:30:37 +0530

    Add solution for Set Mismatch

---
+++ solutions/easy/Set Mismatch.java
@@ -0,0 +1,21 @@
+// Title: Set Mismatch
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/set-mismatch/
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1){
+                return new int[] {nums[index], 
+                index+1};
