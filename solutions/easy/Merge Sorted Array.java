aecdc21cc57cc06a1fd14f7aa819c0b1c697dbab
basu_kalakoti <basukalakoti@gmail.com>
2026-09-13 19:23:24 +0530

    Add solution for Merge Sorted Array

--- solutions/easy/Merge Sorted Array.java
+++ solutions/easy/Merge Sorted Array.java
@@ -4,14 +4,18 @@
             // Link: https://leetcode.com/problems/merge-sorted-array/
 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+//non optimized and o(n log n) time commplexity 
+    // // Copy nums2 into nums1
+    // for (int i = 0; i < n; i++) {
+    //     nums1[m + i] = nums2[i];
+    // }
 
-    // Copy nums2 into nums1
-    for (int i = 0; i < n; i++) {
-        nums1[m + i] = nums2[i];
-    }
+    // // Sort the complete nums1 array
+    // Arrays.sort(nums1);
 
-    // Sort the complete nums1 array
-    Arrays.sort(nums1);
-}
-}
+//optimized solution
+int i=m-1;
+int j=n-1;
+int k=m+n-1;
