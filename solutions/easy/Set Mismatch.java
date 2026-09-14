62a73ef8ab2a2987e0206e64d84ef1b7b2636426
basu_kalakoti <basukalakoti@gmail.com>
2026-09-14 16:07:57 +0530

    Add solution for Set Mismatch

--- solutions/easy/Set Mismatch.java
+++ solutions/easy/Set Mismatch.java
@@ -5,6 +5,7 @@
 
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        //apply  cyclic  sort method 
         int i=0;
         while(i<nums.length){
             int correct=nums[i]-1;
@@ -17,5 +18,3 @@ class Solution {
         }
         for(int index=0;index<nums.length;index++){
             if(nums[index]!=index+1){
-                return new int[] {nums[index], 
-                index+1};
