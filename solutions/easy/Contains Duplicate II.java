afba6acb4c10f2908b1e925b03d16770408326da
basu_kalakoti <basukalakoti@gmail.com>
2026-09-21 21:16:32 +0530

    Add solution for Contains Duplicate II

---
+++ solutions/easy/Contains Duplicate II.java
@@ -0,0 +1,21 @@
+// Title: Contains Duplicate II
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/contains-duplicate-ii/
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!hm.containsKey(nums[i]))
+            {
+                hm.put(nums[i], i);
+            }
+            else{
+                int pastindex=hm.get(nums[i]);
+                if(Math.abs(pastindex-i)<=k){
+                    return true;
+                }
+                else{
+                    hm.put(nums[i], i);
+                }
