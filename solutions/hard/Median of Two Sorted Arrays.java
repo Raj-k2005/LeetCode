9697fcd961c4af5e21524607e500f7e7959fdaf7
basu_kalakoti <basukalakoti@gmail.com>
2026-09-22 20:37:18 +0530

    Add solution for Median of Two Sorted Arrays

---
+++ solutions/hard/Median of Two Sorted Arrays.java
@@ -0,0 +1,22 @@
+// Title: Median of Two Sorted Arrays
+            // Difficulty: Hard
+            // Language: Java
+            // Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
+
+        while (i < nums1.length) {
+            merged[k++] = nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            merged[k++] = nums2[j++];
+        }
+
