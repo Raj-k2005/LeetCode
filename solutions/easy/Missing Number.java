a3790f5f599eb8b130568dc3cb1e50a759d862ae
basu_kalakoti <basukalakoti@gmail.com>
2026-09-23 19:43:33 +0530

    Add solution for Missing Number

--- solutions/easy/Missing Number.java
+++ solutions/easy/Missing Number.java
@@ -4,18 +4,18 @@
             // Link: https://leetcode.com/problems/missing-number/
 
             }
-            if(arr[i]<arr.length && arr[i]!=arr[cur]){
-                swap(arr, i, cur);
-        int i=0;
-        while(i<arr.length){
-            int cur=arr[i];
-        // int missingnum=actualsum-sum;
+        }
 
-        // return missingnum;
-        // }
-        // int actualsum=(nums.length*(nums.length+1))/2;
-        // int sum=0;
-        // for(int i=0;i<nums.length;i++){
-        //     sum=sum+nums[i];
-class Solution {
-    public int missingNumber(int[] arr) {
+        // If nothing is missing from 0 to n-1,
+        // then n is missing
+        return arr.length;
+    }
+
+        static void swap(int[] arr, int first, int 
+        sec){
+        int temp=arr[first];
+        arr[first]=arr[sec];
+        arr[sec]=temp;
+    }
+    }
+
