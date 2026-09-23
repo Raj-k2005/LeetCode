ea59e7513d8f4c3e33cf8e2f1cff3c2af1a1ac1b
basu_kalakoti <basukalakoti@gmail.com>
2026-09-23 19:35:09 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.java
+++ solutions/easy/Two Sum.java
@@ -9,11 +9,13 @@ class Solution {
         for (int i = 0; i <= nums.length; i++) {
             int complement = target - nums[i];
             if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+                return new int[] { map.get
+                (complement), i };
             } else {
                 map.put(nums[i], i);
             }
         }
-        throw new IllegalArgumentException("no match");
+        throw new IllegalArgumentException("no 
+        match");
     }
 }
