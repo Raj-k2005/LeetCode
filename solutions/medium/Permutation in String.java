af0014dbb568a0674c38de36a0c097e08f5cc885
basu_kalakoti <basukalakoti@gmail.com>
2026-09-23 19:42:10 +0530

    Add solution for Permutation in String

--- solutions/medium/Permutation in String.java
+++ solutions/medium/Permutation in String.java
@@ -3,17 +3,19 @@
             // Language: Java
             // Link: https://leetcode.com/problems/permutation-in-string/
 
-           if(isMatched(map1, map2)){
+class Solution {
+    public boolean checkInclusion(String s1, String 
+    s2) {
+        int n=s1.length();
+        int m=s2.length();
+        if(m<n) return false;
+        int map1[]= new int[26];
+        int map2[]= new int[26];
+
+        for(int i=0;i<s1.length();i++){
+            map1[s1.charAt(i)-'a']++;
+            map2[s2.charAt(i)-'a']++;
+
+        }
+        if(isMatched(map1, map2)){
             return true;
-           }
-        }
-        return false;
-    }
-    private boolean isMatched(int map[], int 
-    map2[]){
-        for(int i=0;i<26;i++){
-            if(map[i]!=map2[i]) return false;
-        }
-        return true;
-    }
-}
