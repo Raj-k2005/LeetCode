2cad05922cc7c747204962688cede29f82b94733
basu_kalakoti <basukalakoti@gmail.com>
2026-09-25 22:01:39 +0530

    Add solution for Reverse Vowels of a String

--- solutions/easy/Reverse Vowels of a String.java
+++ solutions/easy/Reverse Vowels of a String.java
@@ -3,19 +3,21 @@
             // Language: Java
             // Link: https://leetcode.com/problems/reverse-vowels-of-a-string/
 
-class Solution {
-    public String reverseVowels(String s) {
-        int n=s.length();
-        char ch[]=s.toCharArray();
-        int start=0;
-        int end=s.length()-1;
-
-        while(start<end){
-            if(!isVowel(ch[start]))
             {
                 start++;
             }
-            else if(!isVowel(ch[end])){
+            else if(!isVowel(ch[end])){ // if end is not vowel then --
                 end--;
             }
-            else{
+            else{ // both are vowels then swap both points
+                char temp=ch[start];
+                ch[start]=ch[end];
+            if(!isVowel(ch[start]))
+
+        while(start<end){ //if start is not vowel then ++
+        int start=0;
+        int end=s.length()-1;
+    public String reverseVowels(String s) {
+        int n=s.length();
+        char ch[]=s.toCharArray();
+class Solution {
